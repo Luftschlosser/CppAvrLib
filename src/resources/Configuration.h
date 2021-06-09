bfe38ffb0b4b4e9625fc41341c735ce3ef31c720
Valentin Felder <valentin.felder@stud.htwk-leipzig.de>
2021-06-09 22:27:43 +0200

    Cleaned up Interrupt Configuration

--- src/resources/Configuration.h
+++ src/resources/Configuration.h
@@ -1,49 +1,34 @@
 #ifndef SRC_RESOURCES_CONFIGURATION_H_
 #define SRC_RESOURCES_CONFIGURATION_H_
 
-//_____________________________________________
-//BEHAVIOR:
 
 namespace Configuration {
 
 	///Flag to enable/disable periphery runtime allocations
 	static constexpr bool runtimeAllocationsEnabled = true;
 
+	//Flags to enable/disable Interrupt-Instantiations (change "ISR.cpp" to remove ISR-routines completely)
+	static constexpr bool enableInterrupt_EXT_INT0 		= true;
+	static constexpr bool enableInterrupt_EXT_INT1 		= false;
+	static constexpr bool enableInterrupt_EXT_INT2 		= false;
+	static constexpr bool enableInterrupt_EXT_INT3 		= false;
+	static constexpr bool enableInterrupt_EXT_INT4 		= false;
+	static constexpr bool enableInterrupt_EXT_INT5 		= false;
+	static constexpr bool enableInterrupt_EXT_INT6 		= false;
+	static constexpr bool enableInterrupt_EXT_INT7 		= false;
+	static constexpr bool enableInterrupt_USART0_RX 	= true;
+	static constexpr bool enableInterrupt_USART0_TX 	= true;
+	static constexpr bool enableInterrupt_USART0_UDRE 	= true;
+	static constexpr bool enableInterrupt_USART1_RX 	= false;
+	static constexpr bool enableInterrupt_USART1_TX 	= false;
+	static constexpr bool enableInterrupt_USART1_UDRE 	= false;
+	static constexpr bool enableInterrupt_USART2_RX 	= false;
+	static constexpr bool enableInterrupt_USART2_TX 	= false;
+	static constexpr bool enableInterrupt_USART2_UDRE 	= false;
+	static constexpr bool enableInterrupt_USART3_RX 	= false;
+	static constexpr bool enableInterrupt_USART3_TX 	= false;
+	static constexpr bool enableInterrupt_USART3_UDRE 	= false;
 }
-
-//_____________________________________________
-//INTERRUPT-TYPES:
-
-//Types of ISR-Routine-Implementations
-#define NONE 0
-#define BLOCK 1
-#define NOBLOCK 2
-
-//_____________________________________________
-//INTERRUPT-CONFIGURATION:
-
-//Implementation Type configuration for each Interrupt source [NONE / BLOCK / NOBLOCK]
-#define ISRTYPE_EXT_INT0 BLOCK
-#define ISRTYPE_EXT_INT1 NONE
-#define ISRTYPE_EXT_INT2 NONE
-#define ISRTYPE_EXT_INT3 NONE
-#define ISRTYPE_EXT_INT4 NONE
-#define ISRTYPE_EXT_INT5 NONE
-#define ISRTYPE_EXT_INT6 NONE
-#define ISRTYPE_EXT_INT7 NONE
-
-#define ISRTYPE_USART0_RX BLOCK
-#define ISRTYPE_USART0_TX BLOCK
-#define ISRTYPE_USART0_UDRE BLOCK
-#define ISRTYPE_USART1_RX NONE
-#define ISRTYPE_USART1_TX NONE
-#define ISRTYPE_USART1_UDRE NONE
-#define ISRTYPE_USART2_RX NONE
-#define ISRTYPE_USART2_TX NONE
-#define ISRTYPE_USART2_UDRE NONE
-#define ISRTYPE_USART3_RX NONE
-#define ISRTYPE_USART3_TX NONE
-#define ISRTYPE_USART3_UDRE NONE
 
 
 #endif /* SRC_RESOURCES_CONFIGURATION_H_ */
--- src/resources/m2560/ISRs.cpp
+++ src/resources/m2560/ISRs.cpp
@@ -2,7 +2,44 @@
 #include "../Configuration.h"
 #include "../interrupts/Interrupt.h"
 
-//Empty ISR-Routine: 82 Byte
+
+//ISR-Routine (enabled)	: 98B ROM + 4B RAM
+//ISR-Routine (empty)	: 20B ROM + 0B RAM
+
+
+//_____________________________________________
+//INTERRUPT-TYPES:
+
+//Types of ISR-Routine-Implementations
+#define NONE 	0
+#define BLOCK 	1
+#define NOBLOCK	2
+
+//_____________________________________________
+//INTERRUPT-CONFIGURATION:
+
+//Implementation Type configuration for each Interrupt source [NONE / BLOCK / NOBLOCK]
+#define ISRTYPE_EXT_INT0 	BLOCK
+#define ISRTYPE_EXT_INT1 	BLOCK
+#define ISRTYPE_EXT_INT2 	BLOCK
+#define ISRTYPE_EXT_INT3 	BLOCK
+#define ISRTYPE_EXT_INT4 	BLOCK
+#define ISRTYPE_EXT_INT5 	BLOCK
+#define ISRTYPE_EXT_INT6 	BLOCK
+#define ISRTYPE_EXT_INT7 	BLOCK
+
+#define ISRTYPE_USART0_RX 	BLOCK
+#define ISRTYPE_USART0_TX 	BLOCK
+#define ISRTYPE_USART0_UDRE	BLOCK
+#define ISRTYPE_USART1_RX 	BLOCK
+#define ISRTYPE_USART1_TX 	BLOCK
+#define ISRTYPE_USART1_UDRE	BLOCK
+#define ISRTYPE_USART2_RX 	BLOCK
+#define ISRTYPE_USART2_TX 	BLOCK
+#define ISRTYPE_USART2_UDRE	BLOCK
+#define ISRTYPE_USART3_RX 	BLOCK
+#define ISRTYPE_USART3_TX 	BLOCK
+#define ISRTYPE_USART3_UDRE	BLOCK
 
 
 //___________________________________
@@ -12,13 +49,15 @@
 #if ISRTYPE_EXT_INT0 == BLOCK
 
 ISR(INT0_vect, ISR_BLOCK) {
-	Interrupt::invoke<INT0_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT0)
+		Interrupt::invoke<INT0_vect_num>();
 }
 
 #elif ISRTYPE_EXT_INT0 == NOBLOCK
 
 ISR(INT0_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<INT0_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT0)
+		Interrupt::invoke<INT0_vect_num>();
 }
 
 #endif
@@ -28,13 +67,15 @@ ISR(INT0_vect, ISR_NOBLOCK) {
 #if ISRTYPE_EXT_INT1 == BLOCK
 
 ISR(INT1_vect, ISR_BLOCK) {
-	Interrupt::invoke<INT1_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT1)
+		Interrupt::invoke<INT1_vect_num>();
 }
 
 #elif ISRTYPE_EXT_INT1 == NOBLOCK
 
 ISR(INT1_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<INT1_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT1)
+		Interrupt::invoke<INT1_vect_num>();
 }
 
 #endif
@@ -44,13 +85,15 @@ ISR(INT1_vect, ISR_NOBLOCK) {
 #if ISRTYPE_EXT_INT2 == BLOCK
 
 ISR(INT2_vect, ISR_BLOCK) {
-	Interrupt::invoke<INT2_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT2)
+		Interrupt::invoke<INT2_vect_num>();
 }
 
 #elif ISRTYPE_EXT_INT2 == NOBLOCK
 
 ISR(INT2_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<INT2_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT2)
+		Interrupt::invoke<INT2_vect_num>();
 }
 
 #endif
@@ -60,13 +103,15 @@ ISR(INT2_vect, ISR_NOBLOCK) {
 #if ISRTYPE_EXT_INT3 == BLOCK
 
 ISR(INT3_vect, ISR_BLOCK) {
-	Interrupt::invoke<INT3_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT3)
+		Interrupt::invoke<INT3_vect_num>();
 }
 
 #elif ISRTYPE_EXT_INT3 == NOBLOCK
 
 ISR(INT3_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<INT3_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT3)
+		Interrupt::invoke<INT3_vect_num>();
 }
 
 #endif
@@ -76,13 +121,15 @@ ISR(INT3_vect, ISR_NOBLOCK) {
 #if ISRTYPE_EXT_INT4 == BLOCK
 
 ISR(INT4_vect, ISR_BLOCK) {
-	Interrupt::invoke<INT4_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT4)
+		Interrupt::invoke<INT4_vect_num>();
 }
 
 #elif ISRTYPE_EXT_INT4 == NOBLOCK
 
 ISR(INT4_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<INT4_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT4)
+		Interrupt::invoke<INT4_vect_num>();
 }
 
 #endif
@@ -92,13 +139,15 @@ ISR(INT4_vect, ISR_NOBLOCK) {
 #if ISRTYPE_EXT_INT5 == BLOCK
 
 ISR(INT5_vect, ISR_BLOCK) {
-	Interrupt::invoke<INT5_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT5)
+		Interrupt::invoke<INT5_vect_num>();
 }
 
 #elif ISRTYPE_EXT_INT5 == NOBLOCK
 
 ISR(INT5_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<INT5_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT5)
+		Interrupt::invoke<INT5_vect_num>();
 }
 
 #endif
@@ -108,13 +157,15 @@ ISR(INT5_vect, ISR_NOBLOCK) {
 #if ISRTYPE_EXT_INT6 == BLOCK
 
 ISR(INT6_vect, ISR_BLOCK) {
-	Interrupt::invoke<INT6_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT6)
+		Interrupt::invoke<INT6_vect_num>();
 }
 
 #elif ISRTYPE_EXT_INT6 == NOBLOCK
 
 ISR(INT6_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<INT6_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT6)
+		Interrupt::invoke<INT6_vect_num>();
 }
 
 #endif
@@ -124,13 +175,15 @@ ISR(INT6_vect, ISR_NOBLOCK) {
 #if ISRTYPE_EXT_INT7 == BLOCK
 
 ISR(INT7_vect, ISR_BLOCK) {
-	Interrupt::invoke<INT7_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT7)
+		Interrupt::invoke<INT7_vect_num>();
 }
 
 #elif ISRTYPE_EXT_INT7 == NOBLOCK
 
 ISR(INT7_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<INT7_vect_num>();
+	if (Configuration::enableInterrupt_EXT_INT7)
+		Interrupt::invoke<INT7_vect_num>();
 }
 
 #endif
@@ -144,13 +197,15 @@ ISR(INT7_vect, ISR_NOBLOCK) {
 #if ISRTYPE_USART0_RX == BLOCK
 
 ISR(USART0_RX_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART0_RX_vect_num>();
+	if (Configuration::enableInterrupt_USART0_RX)
+		Interrupt::invoke<USART0_RX_vect_num>();
 }
 
 #elif ISRTYPE_USART0_RX == NOBLOCK
 
 ISR(USART0_RX_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART0_RX_vect_num>();
+	if (Configuration::enableInterrupt_USART0_RX)
+		Interrupt::invoke<USART0_RX_vect_num>();
 }
 
 #endif
@@ -161,13 +216,15 @@ ISR(USART0_RX_vect, ISR_NOBLOCK) {
 #if ISRTYPE_USART1_RX == BLOCK
 
 ISR(USART1_RX_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART1_RX_vect_num>();
+	if (Configuration::enableInterrupt_USART1_RX)
+		Interrupt::invoke<USART1_RX_vect_num>();
 }
 
 #elif ISRTYPE_USART1_RX == NOBLOCK
 
 ISR(USART1_RX_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART1_RX_vect_num>();
+	if (Configuration::enableInterrupt_USART1_RX)
+		Interrupt::invoke<USART1_RX_vect_num>();
 }
 
 #endif
@@ -178,13 +235,15 @@ ISR(USART1_RX_vect, ISR_NOBLOCK) {
 #if ISRTYPE_USART2_RX == BLOCK
 
 ISR(USART2_RX_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART2_RX_vect_num>();
+	if (Configuration::enableInterrupt_USART2_RX)
+		Interrupt::invoke<USART2_RX_vect_num>();
 }
 
 #elif ISRTYPE_USART2_RX == NOBLOCK
 
 ISR(USART2_RX_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART2_RX_vect_num>();
+	if (Configuration::enableInterrupt_USART2_RX)
+		Interrupt::invoke<USART2_RX_vect_num>();
 }
 
 #endif
@@ -195,13 +254,15 @@ ISR(USART2_RX_vect, ISR_NOBLOCK) {
 #if ISRTYPE_USART3_RX == BLOCK
 
 ISR(USART3_RX_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART3_RX_vect_num>();
+	if (Configuration::enableInterrupt_USART3_RX)
+		Interrupt::invoke<USART3_RX_vect_num>();
 }
 
 #elif ISRTYPE_USART3_RX == NOBLOCK
 
 ISR(USART3_RX_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART3_RX_vect_num>();
+	if (Configuration::enableInterrupt_USART3_RX)
+		Interrupt::invoke<USART3_RX_vect_num>();
 }
 
 #endif
@@ -216,64 +277,72 @@ ISR(USART3_RX_vect, ISR_NOBLOCK) {
 #if ISRTYPE_USART0_TX == BLOCK
 
 ISR(USART0_TX_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART0_TX_vect_num>();
+	if (Configuration::enableInterrupt_USART0_TX)
+		Interrupt::invoke<USART0_TX_vect_num>();
 }
 
 #elif ISRTYPE_USART0_TX == NOBLOCK
 
 ISR(USART0_TX_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART0_TX_vect_num>();
+	if (Configuration::enableInterrupt_USART0_TX)
+		Interrupt::invoke<USART0_TX_vect_num>();
 }
 
 #endif
 #endif
 
-//USART1_RX
+//USART1_TX
 #if ISRTYPE_USART1_TX != NONE
 #if ISRTYPE_USART1_TX == BLOCK
 
 ISR(USART1_TX_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART1_TX_vect_num>();
+	if (Configuration::enableInterrupt_USART1_TX)
+		Interrupt::invoke<USART1_TX_vect_num>();
 }
 
 #elif ISRTYPE_USART1_TX == NOBLOCK
 
 ISR(USART1_TX_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART1_TX_vect_num>();
+	if (Configuration::enableInterrupt_USART1_TX)
+		Interrupt::invoke<USART1_TX_vect_num>();
 }
 
 #endif
 #endif
 
-//USART2_RX
+//USART2_TX
 #if ISRTYPE_USART2_TX != NONE
 #if ISRTYPE_USART2_TX == BLOCK
 
 ISR(USART2_TX_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART2_TX_vect_num>();
+	if (Configuration::enableInterrupt_USART2_TX)
+		Interrupt::invoke<USART2_TX_vect_num>();
 }
 
 #elif ISRTYPE_USART2_TX == NOBLOCK
 
 ISR(USART2_TX_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART2_TX_vect_num>();
+	if (Configuration::enableInterrupt_USART2_TX)
+		Interrupt::invoke<USART2_TX_vect_num>();
 }
 
 #endif
 #endif
 
-//USART3_RX
+//USART3_TX
 #if ISRTYPE_USART3_TX != NONE
 #if ISRTYPE_USART3_TX == BLOCK
 
 ISR(USART3_TX_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART3_TX_vect_num>();
+	if (Configuration::enableInterrupt_USART3_TX)
+		Interrupt::invoke<USART3_TX_vect_num>();
 }
 
 #elif ISRTYPE_USART3_TX == NOBLOCK
 
 ISR(USART3_TX_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART3_TX_vect_num>();
+	if (Configuration::enableInterrupt_USART3_TX)
+		Interrupt::invoke<USART3_TX_vect_num>();
 }
 
 #endif
@@ -288,64 +357,72 @@ ISR(USART3_TX_vect, ISR_NOBLOCK) {
 #if ISRTYPE_USART0_UDRE == BLOCK
 
 ISR(USART0_UDRE_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART0_UDRE_vect_num>();
+	if (Configuration::enableInterrupt_USART0_UDRE)
+		Interrupt::invoke<USART0_UDRE_vect_num>();
 }
 
 #elif ISRTYPE_USART0_UDRE == NOBLOCK
 
 ISR(USART0_UDRE_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART0_UDRE_vect_num>();
+	if (Configuration::enableInterrupt_USART0_UDRE)
+		Interrupt::invoke<USART0_UDRE_vect_num>();
 }
 
 #endif
 #endif
 
-//USART1_RX
+//USART1_UDRE
 #if ISRTYPE_USART1_UDRE != NONE
 #if ISRTYPE_USART1_UDRE == BLOCK
 
 ISR(USART1_UDRE_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART1_UDRE_vect_num>();
+	if (Configuration::enableInterrupt_USART1_UDRE)
+		Interrupt::invoke<USART1_UDRE_vect_num>();
 }
 
 #elif ISRTYPE_USART1_UDRE == NOBLOCK
 
 ISR(USART1_UDRE_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART1_UDRE_vect_num>();
+	if (Configuration::enableInterrupt_USART1_UDRE)
+		Interrupt::invoke<USART1_UDRE_vect_num>();
 }
 
 #endif
 #endif
 
-//USART2_RX
+//USART2_UDRE
 #if ISRTYPE_USART2_UDRE != NONE
 #if ISRTYPE_USART2_UDRE == BLOCK
 
 ISR(USART2_UDRE_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART2_UDRE_vect_num>();
+	if (Configuration::enableInterrupt_USART2_UDRE)
+		Interrupt::invoke<USART2_UDRE_vect_num>();
 }
 
 #elif ISRTYPE_USART2_UDRE == NOBLOCK
 
 ISR(USART2_UDRE_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART2_UDRE_vect_num>();
+	if (Configuration::enableInterrupt_USART2_UDRE)
+		Interrupt::invoke<USART2_UDRE_vect_num>();
 }
 
 #endif
 #endif
 
-//USART3_RX
+//USART3_UDRE
 #if ISRTYPE_USART3_UDRE != NONE
 #if ISRTYPE_USART3_UDRE == BLOCK
 
 ISR(USART3_UDRE_vect, ISR_BLOCK) {
-	Interrupt::invoke<USART3_UDRE_vect_num>();
+	if (Configuration::enableInterrupt_USART3_UDRE)
+		Interrupt::invoke<USART3_UDRE_vect_num>();
 }
 
 #elif ISRTYPE_USART3_UDRE == NOBLOCK
 
 ISR(USART3_UDRE_vect, ISR_NOBLOCK) {
-	Interrupt::invoke<USART3_UDRE_vect_num>();
+	if (Configuration::enableInterrupt_USART3_UDRE)
+		Interrupt::invoke<USART3_UDRE_vect_num>();
 }
 
 #endif
