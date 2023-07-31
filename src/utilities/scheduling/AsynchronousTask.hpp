79cd28a17c75d0377b26d05c2d1a8ab9406cc68a
Valentin Felder <valentin.felder@stud.htwk-leipzig.de>
2023-07-31 03:39:22 +0200

    Added optional return types to AsynchronousTask

--- src/utilities/scheduling/AsynchronousTask.hpp
+++ src/utilities/scheduling/AsynchronousTask.hpp
@@ -15,7 +15,8 @@ enum AsynchronousTaskExecutionBehavior {
 
 ///An instance of this class can store one function- or method call to be executed later.
 ///It is useful to schedule a task (for example in an ISR) to be executed later (for example in the main loop).
-class AsynchronousTask {
+///\param ReturnType This template parameter specifies the return type of the functions and methods wrapped by this AsynchronousTask.
+template <typename ReturnType> class AsynchronousTask {
 
 private:
 
@@ -26,7 +27,7 @@ private:
 		void* obj;
 
 		///The pointer to the functions which wraps the actual function- or method call.
-		void (*runner)(void* obj);
+		ReturnType (*runner)(void* obj);
 	};
 
 	///The \see TaskData Instance used by this AsynchronousTask Instance.
@@ -34,20 +35,20 @@ private:
 
 
 	///Wrapper to call class-methods.
-	template<class T, void(T::*method)()> static void callMethod(void* obj) noexcept {
+	template<class T, ReturnType(T::*method)()> static ReturnType callMethod(void* obj) noexcept {
 		T* t = static_cast<T*>(obj);
-		(t->*method)();
+		return (t->*method)();
 	}
 
 	///Wrapper to call function with one argument.
-	template<class T, void(*function)(T& data)> static void callFunction(void* obj) noexcept {
+	template<class T, ReturnType(*function)(T* data)> static ReturnType callFunction(void* obj) noexcept {
 		T* t = static_cast<T*>(obj);
-		function(*t);
+		return function(t);
 	}
 
 	///Wrapper to call function with no argument.
-	template<void(*function)()> static void callFunction(void* obj) noexcept {
-		function();
+	template<ReturnType(*function)()> static ReturnType callFunction(void* obj) noexcept {
+		return function();
 	}
 
 public:
@@ -66,7 +67,7 @@ public:
 	///\param T This template parameter specifies the class of which the method is part of.
 	///\param method This template parameter specifies the method (at compiletime).
 	///\param object A reference to the object upon which the method shall be called.
-	template<class T, void (T::*method)()> inline void scheduleMethod(T& object) noexcept {
+	template<class T, ReturnType (T::*method)()> inline void scheduleMethod(T& object) noexcept {
 		ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
 			this->target.obj = &object;
 			this->target.runner = &callMethod<T, method>;
@@ -78,16 +79,16 @@ public:
 	///\param T This template parameter specifies the type of argument the function takes.
 	///\param function This template parameter specifies the function (at compiletime).
 	///\param data A reference to the argument with which the function shall be called.
-	template<typename T, void (*function)(T& data)> inline void scheduleFunction(T& data) noexcept {
+	template<typename T, ReturnType (*function)(T* data)> inline void scheduleFunction(T* data) noexcept {
 		ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-			this->target.obj = &data;
+			this->target.obj = data;
 			this->target.runner = &callFunction<T, function>;
 		}
 	}
 
 	///Schedules a function which takes no argument.
 	///\param function This template parameter specifies the function (at compiletime).
-	template<void (*function)()> inline void scheduleFunction() noexcept {
+	template<ReturnType (*function)()> inline void scheduleFunction() noexcept {
 		ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
 			this->target.runner = &callFunction<function>;
 		}
@@ -95,7 +96,7 @@ public:
 
 	///Executes the scheduled function or method.
 	///\param Behavior This template parameter specifies whether the task will be cleared after execution or if the task will remain scheduled to be executed multiple times. [ONCE / REPEATEDLY].
-	template <AsynchronousTaskExecutionBehavior Behavior> inline void execute() noexcept {
+	template <AsynchronousTaskExecutionBehavior Behavior> inline ReturnType execute() noexcept {
 		TaskData tmp;
 		ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
 			tmp = this->target;
@@ -104,7 +105,10 @@ public:
 			}
 		}
 		if (tmp.runner != nullptr) {
-			tmp.runner(tmp.obj);
+			return tmp.runner(tmp.obj);
+		}
+		else {
+			return ReturnType();
 		}
 	}
 };
