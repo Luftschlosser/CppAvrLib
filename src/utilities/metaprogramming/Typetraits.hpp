46519c9788e503df2270e144bdcd27c04eaf99f5
Valentin Felder <valentin.felder@stud.htwk-leipzig.de>
2024-06-10 22:59:42 +0200

    Added Typetraits (similar to std::type_traits)

---
+++ src/utilities/metaprogramming/Typetraits.hpp
@@ -0,0 +1,22 @@
+#ifndef SRC_UTILITIES_METAPROGRAMMING_TYPETRAITS_HPP_
+#define SRC_UTILITIES_METAPROGRAMMING_TYPETRAITS_HPP_
+
+
+namespace Typetraits {
+
+	template<class T> struct remove_cv { typedef T type; };
+	template<class T> struct remove_cv<const T> { typedef T type; };
+	template<class T> struct remove_cv<volatile T> { typedef T type; };
+	template<class T> struct remove_cv<const volatile T> { typedef T type; };
+
+	template<class T> struct remove_const { typedef T type; };
+	template<class T> struct remove_const<const T> { typedef T type; };
+
+	template<class T> struct remove_volatile { typedef T type; };
+	template<class T> struct remove_volatile<volatile T> { typedef T type; };
+
+	//Todo: Add more functions as needed, copy from <std::type_traits>
+}
+
+
+#endif /* SRC_UTILITIES_METAPROGRAMMING_TYPETRAITS_HPP_ */
