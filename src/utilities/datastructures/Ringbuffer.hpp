08e017881305c341777fd1f05aae9d89c7a74241
Valentin Felder <valentin.felder@stud.htwk-leipzig.de>
2024-06-10 22:17:41 +0200

    Implemented Ringbuffer datastructure

---
+++ src/utilities/datastructures/Ringbuffer.hpp
@@ -0,0 +1,34 @@
+#ifndef SRC_UTILITIES_DATASTRUCTURES_RINGBUFFER_HPP_
+#define SRC_UTILITIES_DATASTRUCTURES_RINGBUFFER_HPP_
+
+
+#include <stdint.h>
+
+
+template<typename T, uintptr_t Size> class Ringbuffer {
+private:
+	volatile uintptr_t inputIndex;
+	uintptr_t outputIndex;
+	T buffer[Size];
+
+public:
+	Ringbuffer() noexcept : inputIndex(0), outputIndex(0) {}
+
+	inline bool inputNewElement(T element) noexcept {
+		this->inputIndex = (this->inputIndex + 1) % Size;
+		this->buffer[this->inputIndex] = element;
+		return (this->inputIndex != this->outputIndex);
+	}
+
+	inline bool hasNextElement() noexcept {
+		return (this->inputIndex != this->outputIndex);
+	}
+
+	inline T getNextElement() noexcept {
+		this->outputIndex = (this->outputIndex + 1) % Size;
+		return this->buffer[this->outputIndex];
+	}
+};
+
+
+#endif /* SRC_UTILITIES_DATASTRUCTURES_RINGBUFFER_HPP_ */
