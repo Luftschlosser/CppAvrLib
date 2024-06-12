efae4098395b771eec3452effdf36673a84610f3
Valentin Felder <valentin.felder@stud.htwk-leipzig.de>
2024-06-12 04:09:58 +0200

    Some some advanced functionality to Bitmask

--- src/utilities/datastructures/Bitmask.hpp
+++ src/utilities/datastructures/Bitmask.hpp
@@ -9,13 +9,29 @@ template<typename AddressType, AddressType Size> class Bitmask {
 
 private:
 
-	uint8_t mask[(Size+7)/8];
+	static constexpr AddressType arraySize = (Size+7)/8;
+
+	uint8_t mask[arraySize];
 
 public:
 
 	inline Bitmask() noexcept {
 		for (uint8_t i = 0; i < ((Size+7)/8); i++) {
 			this->mask[i] = 0;
+		}
+	}
+
+	inline constexpr AddressType getUnderlyingArraySize() noexcept {
+		return arraySize;
+	}
+
+	inline uint8_t getByte(AddressType byteIndex) noexcept {
+		return this->mask[byteIndex];
+	}
+
+	inline void setByte(AddressType byteIndex, uint8_t byte) noexcept {
+		if (byteIndex < arraySize) {
+			this->mask[byteIndex] = byte;
 		}
 	}
 
@@ -29,6 +45,21 @@ public:
 
 	inline bool isSet(AddressType position) noexcept {
 		return (this->mask[position/8] & (1 << (position % 8)));
+	}
+
+	inline bool hasAnyBitSet() noexcept {
+		for (AddressType i = 0; i < Size; i++) {
+			if (this->mask[i] > 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	inline void clearAllBits() noexcept {
+		for (AddressType i = 0; i < arraySize; i++) {
+			this->mask[i] = 0;
+		}
 	}
 };
 
